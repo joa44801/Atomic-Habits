5383fddd5992868b45c3dfa6b8c2d219b921cc9d
Joanick <joanick.stpierrealain.protic!@gmail.comC:/Users/protic/AppData/Local/Programs/Python/Python38-32/python.exe>
2021-01-15 18:14:18 -0500

    reste bloc

--- main.py
+++ main.py
@@ -10,22 +10,43 @@ def mot_passe():
 
 def date_modif():
     derniere = time.ctime(os.path.getmtime("carnet.txt"))
-    mois = " "
+    mois = 0
     if derniere[4:7] == "Jan":
-        mois == "01"
+        mois =+ 1
     if derniere[4:7] == "Feb":
-        mois == "02"
-    conversion = derniere[20:25] + "-" + mois + "-" + derniere[8:10]
-    return (derniere, conversion)
+        mois =+ 2
+    if derniere[4:7] == "Mar":
+        mois =+ 3
+    if derniere[4:7] == "Apr":
+        mois =+ 4
+    if derniere[4:7] == "May":
+        mois =+ 5
+    if derniere[4:7] == "Jun":
+        mois =+ 6
+    if derniere[4:7] == "Jul":
+        mois =+ 7
+    if derniere[4:7] == "Aug":
+        mois =+ 8
+    if derniere[4:7] == "Sep":
+        mois =+ 9
+    if derniere[4:7] == "Oct":
+        mois =+ 10
+    if derniere[4:7] == "Nov":
+        mois =+ 11
+    if derniere[4:7] == "Dec":
+        mois =+ 12
+    mois = str(mois)
+    if len(mois) < 2:
+        mois = "0" + mois
+    conversion = derniere[20:25] + "-" + str(mois) + "-" + derniere[8:10]
+    return conversion
 
-print(date_modif())
 
-#2021-01-15
 def codex():
     date = str(datetime.date.today())
-    print(date_modif(), type(date_modif()))
-    print(date, type(date))
-    #if date_modif() == date:
-        #print(date_modif(), date)
+    modif = date_modif()
+    code = mot_passe()
+    if date == modif:
+        return code
 
-#codex()
+print(codex())
